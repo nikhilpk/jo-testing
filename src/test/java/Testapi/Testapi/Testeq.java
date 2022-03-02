2d55862d0e55a85c4bc271412b6f2bd627e1564d
nikhilpk <nikhi.pk@gmail.com>
2022-03-02 12:43:50 +0800

    message

--- src/test/java/Testapi/Testapi/Testeq.java
+++ src/test/java/Testapi/Testapi/Testeq.java
@@ -15,7 +15,11 @@ public void postBodyWithJsonString() {
 
   given()
             .baseUri("https://uatapi.equinix.com")
-            .body(data)
+            .body(
+                    "{\"client_id\":\"CZMNesAQ4Avg09YI5iSUngh1FMPtwRH0\",\n" +
+            "    \"client_secret\":\"uoUCcjv9fKkJquhS\",\\n\" +\r\n"
+            + "            \"    \\\"grant_type\\\":\\\"client_credentials\\\"}"
+            )
             .contentType("application/json")
            .when()
             .post("/oauth2/v1/token")
@@ -32,7 +36,7 @@ given()
 .when().get("https://uatapi.equinix.com/v1/orders/workvisit/locations")
 .then().statusCode(200);
 }
-@Test
+//@Test
 public void queryParamTest1() {
 given()
 .queryParam("detail","true")
@@ -42,7 +46,73 @@ given()
 .statusCode(200)
 .log().body()
 .extract().response();
-
-
 }
+
+//@Test
+public void inventory() {
+	given()
+	.when()
+	.get("https://petstore.swagger.io/v2/store/inventory")
+	.then()
+	.statusCode(200)
+	.statusLine("HTTP/1.1 200 OK")
+	.log()
+	.body()
+	.extract()
+	.response();
+	
+}
+@BeforeTest
+public void TestDataSetup()
+{
+File Src = new File(".\src\main\java\Config.Properties");
+FileInputStream fis = new  FileInputStreams(src);
+pro = new properties();
+pro.load(fis);
+}
+RestAssured.baseURI= pro.getProperty("PATH");
+//@Test
+public void inventory1() {
+	given()
+	.when()
+	.get("https://petstore.swagger.io/v2/user/login?username=user&password=admin")
+	.then()
+	.statusCode(200)
+	.statusLine("HTTP/1.1 200 OK")
+	.log()
+	.body()
+	.extract()
+	.response();
+	
+}
+
+@Test
+public void user() {
+	
+	JSONObject user = new JSONObject();
+	user.put("id",0);
+	user.put("username","abc");
+	user.put("firstName","ab");
+	user.put("lastName","cd");
+	user.put("email", "ab@gmail.com");
+	user.put("password","admin");
+	user.put("phone","123");
+	user.put("userStatus",0);
+	given()
+	.body(user)
+	.contentType("application/json")
+	.when()
+	.post("https://petstore.swagger.io/v2/user")
+	.then()
+	.statusCode(200)
+	.statusLine("HTTP/1.1 200 OK")
+	.log()
+	.body()
+	.extract()
+	.response();
+	
+}
+
+
+
 }
